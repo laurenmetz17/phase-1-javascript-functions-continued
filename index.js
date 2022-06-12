4a73b1237eb6fa85ac714bdf0cc648a800db1b6f
Lauren Metz <Laurenmetz15@gmail.com>
2022-06-12 00:41:05 -0400

    commit tree 05047c4cd7b6248a5b2aa2b5416026891ca94352

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+let saturdayFun = function (activity) {
+    activity = typeof activity !== 'undefined' ? activity : 'roller-skate';
+    return `This Saturday, I want to ${activity}!`;
+};
+
+let mondayWork = function (activity = 'go to the office') {
+    activity = typeof activity !== 'undefined' ? activity : 'go to the office';
+    return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(flair = '*') {
+    return function(adjective = 'special') {
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
